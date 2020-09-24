c7d3a63feafb864432c428b13325d5bc68146dde
Quentin Gendre <swynfel@gmail.com>
2020-09-24 19:44:31 +0900

    Improve MonteCarlo by exploring actions with UCB

--- src/players/monte_carlo_player.cpp
+++ src/players/monte_carlo_player.cpp
@@ -10,43 +10,81 @@ MonteCarloPlayer::MonteCarloPlayer(std::shared_ptr<Player> player, bool until_ro
   , rollouts(rollouts)
   , until_round(until_round) {}
 
+// Private
 
 float
-MonteCarloPlayer::game_score(Game& game, const State& state, int player) {
-    float score = 0;
-    for (int k = 0; k < rollouts; k++) {
-        game.override_state(state);
-        game.roll_game();
-        if (game.get_state().winning_player() == player) {
-            score++;
-        }
-    }
-    return score;
+MonteCarloPlayer::end_round_heuristic(int total_score, int player_score, int highest_score, int player_count) {
+    float first_factor = 0.2f;
+    float scale_factor = 0.6f;
+    float score_factor = 1.f - first_factor - scale_factor;
+    return total_score == 0 ? 1.f / player_count
+                            : ((player_score >= highest_score) ? first_factor : 0.f) +
+                                  scale_factor * float(player_score) / highest_score +
+                                  score_factor * float(player_score) / total_score;
 }
 
-
 float
-MonteCarloPlayer::round_score(Game& game, const State& state, int player) {
-    float score = 0;
-    for (int k = 0; k < rollouts; k++) {
-        game.override_state(state);
+MonteCarloPlayer::state_score(Game& game, const State& state, int player) {
+    game.override_state(state);
+    if (until_round) {
         game.roll_round();
         game.end_round();
-        float s = 0;
-        for (int p = 0; p < state.get_rules()->player_count; p++) {
-            s += game.get_state().get_panel(p).get_score();
+        if (game.get_state().is_game_finished()) {
+            game.score_final();
+            return (game.get_state().winning_player() == player) ? 1 : 0;
         }
-        s = game.get_state().get_panel(player).get_score() - (s / state.get_rules()->player_count);
-        score += s;
+        int player_score = 0;
+        int total_score = 0;
+        int highest_score = 0;
+        int player_count = state.get_rules()->player_count;
+        for (int p = 0; p < player_count; p++) {
+            int score = game.get_state().get_panel(p).get_score();
+            total_score += score;
+            if (p == player) {
+                player_score = score;
+            }
+            if (score > highest_score) {
+                highest_score = score;
+            }
+        }
+        return end_round_heuristic(total_score, player_score, highest_score, player_count);
+    } else {
+        game.roll_game();
+        return (game.get_state().winning_player() == player) ? 1 : 0;
     }
-    return score;
+}
+
+// Selects action "a" of index "i" with highest
+// Upper Confidence bound applied to Trees defined as:
+// UCT_i = X_i + c * sqrt( ln(n) / n_i )
+// with:
+//    X_i average score after choosing "a"
+//    c runtime constant, default is sqrt(2)
+//    n total number of samples (n = sum_i(n_i))
+//    n_i number of times action "a" was sampled
+int
+MonteCarloPlayer::select_ucb(int n, const vector<float>& score_sums, const vector<int>& count) const {
+    float ln_n = log(n);
+    float highest_uct = -std::numeric_limits<float>::infinity();
+    int highest_index = 0;
+    for (int i = 0; i < score_sums.size(); i++) {
+        if (count[i] == 0) {
+            return i;
+        }
+        float uct_i = (score_sums[i] / count[i]) + c * sqrt(ln_n / count[i]);
+        if (uct_i > highest_uct) {
+            highest_uct = uct_i;
+            highest_index = i;
+        }
+    }
+    return highest_index;
 }
 
 Action
 MonteCarloPlayer::play(const State& state) {
     int position = state.get_current_player();
     vector<Action> legal_actions;
-    if (true) {
+    if (smart) {
         legal_actions = Game::all_smart_legal(state);
         if (legal_actions.size() == 0) {
             legal_actions = Game::all_penalty_legal(state);
@@ -54,23 +92,43 @@ MonteCarloPlayer::play(const State& state) {
     } else {
         legal_actions = Game::all_legal(state);
     }
-    float best_score = -std::numeric_limits<float>::infinity();
-    Action best_action;
+    vector<float> score_sums(legal_actions.size(), 0);
+    vector<int> count(legal_actions.size(), 0);
 
     Game game = Game(state.get_rules());
     for (int p = 0; p < state.get_rules()->player_count; p++) {
         game.add_player(sampling_player);
     }
 
-    for (Action action : legal_actions) {
+    for (int k = 0; k < rollouts; k++) {
+        int index = select_ucb(k, score_sums, count);
         State next_state(state);
-        Game::apply(action, next_state);
-        float score = until_round ? round_score(game, state, position) : game_score(game, state, position);
+        Game::apply(legal_actions[index], next_state);
+        float score = state_score(game, next_state, position);
+        count[index]++;
+        score_sums[index] += score;
+    }
+
+    float best_score = -std::numeric_limits<float>::infinity();
+    Action best_action;
+    int index = 0;
+    for (Action action : legal_actions) {
+        float score = count[index] > 0 ? score_sums[index] / count[index] : 0.f;
         if (score > best_score) {
             best_score = score;
             best_action = action;
         }
+        index++;
     }
+    // index = 0;
+    // std::cout << ":: ";
+    // for (Action action : legal_actions) {
+    //     float score = count[index] > 0 ? score_sums[index] / count[index] : 0.f;
+    //     std::cout << action;
+    //     printf("(%i|%.3f) ", count[index], score);
+    //     index++;
+    // }
+    // std::cout << std::endl;
     return best_action;
 }
 
--- src/players/monte_carlo_player.hpp
+++ src/players/monte_carlo_player.hpp
@@ -7,18 +7,22 @@
 #include "random_player.hpp"
 #include "state/state.hpp"
 #include "utils/random.hpp"
+#include <cmath>
 
 class MonteCarloPlayer : public Player {
 private:
     std::shared_ptr<Player> sampling_player;
 
-    float round_score(Game& game, const State& state, int player);
-    float game_score(Game& game, const State& state, int player);
+    float end_round_heuristic(int total_score, int player_score, int highest_score, int player_count);
+    float state_score(Game& game, const State& state, int player);
+    int select_ucb(int n, const vector<float>& score_sums, const vector<int>& count) const;
 
 public:
-    const static int DEFAULT_ROLLOUTS = 100;
+    const static int DEFAULT_ROLLOUTS = 10;
     int rollouts;
     bool until_round;
+    bool smart = true;
+    float c = M_SQRT2;
 
     MonteCarloPlayer(bool until_round = true, int rollouts = DEFAULT_ROLLOUTS);
     MonteCarloPlayer(std::shared_ptr<Player> player, bool until_round = true, int rollouts = DEFAULT_ROLLOUTS);
